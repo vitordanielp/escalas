72c366e98fad243f6f646c7f0808568652b95ec2
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2024-02-07 17:20:39 -0300

    dados atualizados

--- atualizar.py
+++ atualizar.py
@@ -3,7 +3,7 @@ from time import sleep
 
 print("Dados sendo atualizados... aguarde.")
 sys("main.py")
-sys("git add .")
+sys("git add *")
 sys('git commit -m "dados atualizados"')
 sys('git push')
 sleep(2)
