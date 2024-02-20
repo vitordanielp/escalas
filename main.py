fbede5fae1b9647cedb1c2e400333c1f5714b033
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2024-02-20 16:43:46 -0300

    add: dados de pacotes de exames adicionados

--- main.py
+++ main.py
@@ -1,52 +1,70 @@
 import pandas as pd
 
-columns = ["data", "sala", "medico", "unidade"]
-df = pd.read_csv("escala.csv", sep=";", header=None, names=columns)
-df["periodo"] = ""
-df["hora"] = ""
+
+################### ESCALAS ###################
+
+def execEscalas():
+    columns = ["data", "sala", "medico", "unidade"]
+    df = pd.read_csv("escala.csv", sep=";", header=None, names=columns)
+    df["periodo"] = ""
+    df["hora"] = ""
 
 
-def extraiHora(data):
-    return data[11:16].replace(":", ".")
+    def extraiHora(data):
+        return data[11:16].replace(":", ".")
 
 
-def limpaData(data):
-    # Data no formato "dd/mm/yyyy"
-    return f"{data[0:2]}/{data[3:5]}/{data[6:10]}"
+    def limpaData(data):
+        # Data no formato "dd/mm/yyyy"
+        return f"{data[0:2]}/{data[3:5]}/{data[6:10]}"
 
 
-def extraiPeriodo(hora):
-    if hora < 13.25:
-        return "MANHA"
-    else:
-        return "TARDE"
+    def extraiPeriodo(hora):
+        if hora < 13.25:
+            return "MANHA"
+        else:
+            return "TARDE"
 
 
-df["hora"] = df["data"].apply(extraiHora).astype(float)
-df["periodo"] = df["hora"].apply(extraiPeriodo)
-df["data"] = df["data"].apply(limpaData)
-del df["hora"]
-df = df.drop_duplicates()
-df = df.sort_values(["data", "sala"])
+    df["hora"] = df["data"].apply(extraiHora).astype(float)
+    df["periodo"] = df["hora"].apply(extraiPeriodo)
+    df["data"] = df["data"].apply(limpaData)
+    del df["hora"]
+    df = df.drop_duplicates()
+    df = df.sort_values(["data", "sala"])
 
 
-df_lago_sul = df.loc[df["unidade"] == "LAGO SUL"]
-df_asa_sul = df.loc[df["unidade"] == "ASA SUL"]
-df_lago_sul = df_lago_sul.reset_index(drop=True)
-df_asa_sul = df_asa_sul.reset_index(drop=True)
+    df_lago_sul = df.loc[df["unidade"] == "LAGO SUL"]
+    df_asa_sul = df.loc[df["unidade"] == "ASA SUL"]
+    df_lago_sul = df_lago_sul.reset_index(drop=True)
+    df_asa_sul = df_asa_sul.reset_index(drop=True)
 
 
-def separar_salas(dataFrame):
-    salas_unidade = dataFrame["sala"].unique()
-    unidade = dataFrame.unidade[0].lower().replace(" ", "-")
-    with open(f"dados/{unidade}/salas.txt", "w") as file:
-        for sala in sorted(salas_unidade):
-            file.writelines(sala + ";")
-    for sala in salas_unidade:
-        new_df = dataFrame.loc[dataFrame["sala"] == sala]
-        new_df.to_json(f"dados/{unidade}/{sala}.json",
-                       index=False, orient="records")
+    def separar_salas(dataFrame):
+        salas_unidade = dataFrame["sala"].unique()
+        unidade = dataFrame.unidade[0].lower().replace(" ", "-")
+        with open(f"dados/{unidade}/salas.txt", "w") as file:
+            for sala in sorted(salas_unidade):
+                file.writelines(sala + ";")
+        for sala in salas_unidade:
+            new_df = dataFrame.loc[dataFrame["sala"] == sala]
+            new_df.to_json(f"dados/{unidade}/{sala}.json",
+                        index=False, orient="records")
+
+    separar_salas(df_lago_sul)
+    separar_salas(df_asa_sul)
+    del df, df_asa_sul, df_lago_sul, columns
+# Fim do script para escalas
 
 
-separar_salas(df_lago_sul)
-separar_salas(df_asa_sul)
+################### PACOTES DE EXAMES ###################
+
+def execPacotes():
+    df = pd.read_csv("pacotes.csv", sep=";", header=None)
+    df = df.fillna(value="-")
+    df.to_json("dados/pacotes/pacotes.json", index=False, orient="records")
+
+
+if __name__ == "__main__":
+    execEscalas()
+    execPacotes()
