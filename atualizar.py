12b40953c69eeb1e097290e44b47d876f703d08c
Vítor Daniel Pontes Guedes <vitordaniel97.vtd@gmail.com>
2024-03-19 14:58:34 -0300

    Revert "Delete atualizar.py"
    This reverts commit 776c4f1b9e7d2bbb2465f5447aa80b2a892aa1d6.

---
+++ atualizar.py
@@ -0,0 +1,11 @@
+from os import system as sys
+from time import sleep
+
+print("Dados sendo atualizados... aguarde.")
+sys("main.py")
+sys("git add *")
+sys('git commit -m "dados atualizados"')
+sys('git push')
+sleep(2)
+print("Finalizado!")
+exit()
